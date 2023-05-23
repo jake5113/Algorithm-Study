15e389d52dbd51b41adcfb9551bfba9a3dd48227
jake <88993810+jake5113@users.noreply.github.com>
2023-05-23 14:07:31 +0900

    [Bronze V] Title: 꼬마 정민, Time: 124 ms, Memory: 13512 KB -BaekjoonHub

---
+++ 백준/Bronze/11382. 꼬마 정민/꼬마 정민.kt
@@ -0,0 +1,10 @@
+import java.util.Scanner
+
+fun main(args: Array<String>) = with(Scanner(System.`in`)) {
+
+    val input1: Long = nextLong()
+    val input2: Long = nextLong()
+    val input3: Long = nextLong()
+
+    println(input1 + input2 + input3)
+}
