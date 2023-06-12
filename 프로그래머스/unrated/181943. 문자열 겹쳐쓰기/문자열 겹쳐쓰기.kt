b9bd4566a8f78c086b78cebb16e442dcdfdfcfc8
jake <88993810+jake5113@users.noreply.github.com>
2023-06-12 16:14:51 +0900

    [unrated] Title: 문자열 겹쳐쓰기, Time: 12.51 ms, Memory: 61.8 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/181943. 문자열 겹쳐쓰기/문자열 겹쳐쓰기.kt
@@ -0,0 +1,7 @@
+class Solution {
+    fun solution(my_string: String, overwrite_string: String, s: Int): String {
+        var answer: String = ""
+        answer = my_string.substring(0, s) + overwrite_string + my_string.substring(s+overwrite_string.length)
+        return answer
+    }
+}
